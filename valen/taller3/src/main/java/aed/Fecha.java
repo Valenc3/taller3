9d232a27ee5b0bc8e4a834e7f5e01adc3ae53cbb
Valentín Cárcamo <142249408+Valenc3@users.noreply.github.com>
2024-04-26 13:52:36 -0300

    Add files via upload

---
+++ valen/taller3/src/main/java/aed/Fecha.java
@@ -0,0 +1,61 @@
+package aed;
+
+public class Fecha {
+     private int _dia;
+     private int _mes;
+    public Fecha(int dia, int mes) {
+        _dia = dia;
+        _mes = mes;
+    }
+
+    public Fecha(Fecha fecha) {
+        _dia = fecha._dia;
+        _mes = fecha._mes;
+    }
+
+    public Integer dia() {
+        return _dia;
+    }
+
+    public Integer mes() {
+        return _mes;
+    }
+
+    public String toString() {
+        Integer d = _dia;
+        Integer m = _mes;
+        return d.toString() + "/" + m.toString();
+    }
+
+    @Override
+    public boolean equals(Object otra) {
+        if (otra.getClass() == this.getClass()){
+            
+
+        
+    }
+
+    public void incrementarDia() {
+        if (_dia == diasEnMes(_mes)){
+            _dia = 1;
+            if (_mes < 12){
+                _mes += 1;
+            }else{
+                _mes = 1;
+            } 
+        }else{
+            _dia += 1;
+        }
+    }
+
+    private int diasEnMes(int mes) {
+        int dias[] = {
+                // ene, feb, mar, abr, may, jun
+                31, 28, 31, 30, 31, 30,
+                // jul, ago, sep, oct, nov, dic
+                31, 31, 30, 31, 30, 31
+        };
+        return dias[mes - 1];
+    }
+
+}
